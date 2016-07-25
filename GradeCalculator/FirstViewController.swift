390be7c1581a6516573ea91884cf57ef0d13e838
Logan Kember <logan.916@hotmail.com>
2016-07-25 15:53:24 -0400

    Initial Commit

---
+++ GradeCalculator/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GradeCalculator
+//
+//  Created by Logan Kember on 2016-07-25.
+//  Copyright © 2016 Logan Kember. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
