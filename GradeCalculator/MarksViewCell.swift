1b8dade58b793f758bcc51fea76a978f41deb57c
Logan Kember <logan.916@hotmail.com>
2016-08-05 16:20:42 -0400

    Adding another view to display course information.

---
+++ GradeCalculator/MarksViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MarksViewCell.swift
+//  GradeCalculator
+//
+//  Created by Logan Kember on 2016-08-05.
+//  Copyright © 2016 Logan Kember. All rights reserved.
+//
+
+import UIKit
+
+class MarksViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
